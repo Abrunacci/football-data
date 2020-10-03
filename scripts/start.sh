dd73568c9585e047d798c4195ec689cfc1571448
Alejandro Brunacci <abrunacci87@gmail.com>
2020-10-03 20:10:11 -0300

    adding database connection and competitions model

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
+set -e
 
+until PGPASSWORD=$POSTGRES_PASS psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_NAME" -c '\q'; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+  
+echo "Postgres is up"
+echo "Executing migrations"
+alembic upgrade head
+echo "Starting server"
 uvicorn source.main:app --reload --host 0.0.0.0
